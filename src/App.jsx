78d6247816ea1bcc5c634aa976e1868378aac33c
kscott2016 <krystinascott2008@yahoo.com>
2024-02-05 17:27:46 -0600

    render starships

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useEffect} from 'react'
+import { Route, Routes } from "react-router-dom" 
+
 import './App.css'
 
+import * as starshipService from "./services/sw-api"
+
+import Starship from './components/Starship'
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [starships, setStarships] = useState([])
+  
+  useEffect(() => {
+    const fetchStarships= async () => {
+      const starshipData = await starshipService.getStarships()
+      console.log('Response from server:', starshipData)
+      setStarships(starshipData.results)
+    }
+    fetchStarships()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* <h1>Test</h1> */}
+      <ul>
+        {starships.map((ship,idx) => (
+          <Starship 
+          key= {idx}
+          starship={ship}
+          />
+        ))}
+      </ul>
     </>
   )
 }
---
+++ src/components/Starship.jsx
@@ -0,0 +1,10 @@
+const Starship =({starship}) => {
+
+  return (
+    <li>
+      <h2>{starship.name}</h2>
+  </li>
+  )
+}
+
+export default Starship
